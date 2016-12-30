1dc43a40eec7f5511a2bb71c874681cea589f317
Derek Jobst <derekjobst@gmail.com>
2016-12-30 16:41:30 -0700

    Initial commit

---
+++ parse.py
@@ -0,0 +1,34 @@
+"""
+Podcast RSS Parser
+
+Parse a podcast RSS feed and print statistics on the publishing time.
+
+Arguments:
+1: String [required] URL of podcast RSS stream
+
+"""
+# Author: Derek Jobst
+# Email: derekjobst@gmail.com
+
+import sys
+import feedparser
+import json
+
+from time import mktime
+from datetime import datetime
+from collections import defaultdict
+
+if __name__ == '__main__':
+    d = feedparser.parse(sys.argv[1])
+
+    days = defaultdict(int)
+
+    # Process entries
+    print 'Processing {} entires'.format(len(d.entries))
+    for entry in d.entries:
+        dt = datetime.fromtimestamp(mktime(entry.published_parsed))
+        days[dt.__format__('%A')] += 1
+
+    # Print Results
+    for d in days:
+        print 'Day {}: {}'.format(d, days[d])
